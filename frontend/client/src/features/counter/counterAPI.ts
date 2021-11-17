9b7dc60df6cba8ec55ea6a0f5dc18ad3e77331e2
jonki324 <jonki324@gmail.com>
2021-11-17 11:41:17 +0900

    不要コード削除

--- frontend/client/src/features/counter/counterAPI.ts
+++
@@ -1,5 +0,0 @@
-export const fetchCount = (amount = 1): Promise<{ data: number }> => {
-  return new Promise<{ data: number }>((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
-};
