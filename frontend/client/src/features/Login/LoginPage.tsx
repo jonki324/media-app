ee5727bd59a24f9711e523b0d971f80a42c354d1
jonki324 <jonki324@gmail.com>
2021-11-18 18:25:00 +0900

    ダミーLoginページ作成

--- frontend/client/src/features/Login/LoginPage.tsx
+++ frontend/client/src/features/Login/LoginPage.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Card, CardContent, Grid, Button, TextField } from '@mui/material';
+import { AccountCircleTwoTone } from '@mui/icons-material';
 import Layout from '../../components/Layout/Layout';
 
 const LoginPage: React.FC = () => {
-  return <Layout>Login Page</Layout>;
+  return (
+    <Layout>
+      <Grid container justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
+        <Grid item>
+          <Card raised sx={{ width: 350 }}>
+            <CardContent>
+              <Grid container spacing={3}>
+                <Grid item xs={12} textAlign="center">
+                  <AccountCircleTwoTone sx={{ fontSize: 70 }} />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField id="loginId" label="Login ID" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="password"
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="contained" fullWidth>
+                    LOGIN
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default LoginPage;
