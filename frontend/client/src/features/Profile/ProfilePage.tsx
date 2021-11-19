639ae0ca0f3e1755e3c79f410660b265db634934
jonki324 <jonki324@gmail.com>
2021-11-19 15:46:20 +0900

    ダミーProfileページ作成

--- frontend/client/src/features/Profile/ProfilePage.tsx
+++ frontend/client/src/features/Profile/ProfilePage.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Grid, Card, CardContent, Typography } from '@mui/material';
+import { Person } from '@mui/icons-material';
 import Layout from '../../components/Layout/Layout';
 
 const ProfilePage: React.FC = () => {
-  return <Layout>Profile Page</Layout>;
+  return (
+    <Layout>
+      <Grid container justifyContent="center" pt={4}>
+        <Grid item>
+          <Card>
+            <Grid container>
+              <Grid item xs={3}>
+                <Person sx={{ width: '100%', height: '100%' }} />
+              </Grid>
+              <Grid item xs={9}>
+                <CardContent>
+                  <Typography variant="h5">Sample User Profile</Typography>
+                  <Typography variant="body2">Sample User Profile Detail</Typography>
+                </CardContent>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default ProfilePage;
